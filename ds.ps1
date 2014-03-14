7c8e2ab9bdb5ee2024ec2c46e4038075d4098881
Dave <eyaswoo@gmail.com>
2014-03-14 10:00:39 +0800

    Update ds.ps1
    add support for POSTing a form

--- ds.ps1
+++ ds.ps1
@@ -4,62 +4,58 @@
 ##
 
 param(
-[int]$port = 8889,
-[int]$max_connection = 50
+[int]$port = 8889
 )
+
+$global:DSHOME = "C:\dshome"
+$global:DSPORT = $port
+#$global:DSSESSION = @{}
+$global:DSPARAMS = @{}
 
 try{
   $server = [system.net.sockets.tcplistener]$port
-} catch [Exception] {
+} catch {
   write-host "cannot open port" $port
   exit
 }
 
-# load function lib
 $execPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 . $execPath\ds_lib.ps1
 
-$server.start($max_connection)
-write-host "server started on" $port
+$server.start()
+write-host "server started at port" $port
 
-while( $true ){
+while( $true ){  
   $client = $server.AcceptTcpClient()
-  write-host "got a request"
+  write-host "Got a request"
   
-  $resp_msg = ""
+  $global:DSPARAMS = @{} # clear params per one connection
+  
   $stream = $client.GetStream()
 
   if($stream.canread){
-    write-host "[Read Request]================================="
+    write-host "[Read Request] " -nonewline
     $avail = $client.available
     write-host $avail "bytes available"
     
     $bytes = new-object system.byte[] $avail
-    $stream.read($bytes, 0, $bytes.length)
-    
+    $stream.read($bytes, 0, $bytes.length)    
     $req_msg = [system.text.encoding]::utf8.getstring($bytes)
-    write-host "host returned:" $req_msg.length "bytes"
             
-    $resp_msg = GenerateMessage($req_msg)
-  }
-  else{
-    write-host "cannot read data"
-    $client.close()
-    $stream.close()
-    continue
-  }
-
-  if($stream.canwrite -and $resp_msg -ne ""){
-    write-host "[Send Response]========================"
-    $msg = [system.text.encoding]::utf8.getbytes($resp_msg)
-    $stream.write($msg, 0, $msg.length)
+    $resp_msg = GenerateMessage $req_msg
+    
+    if($stream.canwrite -and $resp_msg){
+      write-host "[Send Response]"
+      $msg = [system.text.encoding]::utf8.getbytes($resp_msg)
+      $stream.write($msg, 0, $msg.length)
+    }
   }
   
   $client.close()
-  write-host "A request done."
   $stream.close()
+  write-host "A request done."
 }
 
 $server.Stop()
-write-host "server stopped."
+write-host "Server listening loop stopped."
 exit
